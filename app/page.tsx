9b0655a85bad1d38928b13bc5fb1478c63070e20
Koushik Das <koushik78bd@gmail.com>
2023-06-05 22:42:59 +0600

    add board and complete header design

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Board from "@/components/Board";
 import Header from "@/components/Header";
 
 export default function Home() {
@@ -6,8 +7,9 @@ export default function Home() {
       {/* Header  */}
       <Header />
       {/* Board  */}
+      <Board />
 
-      <h1>Trello 2.0 AI Clone</h1>
+      {/* <h1>Trello 2.0 AI Clone</h1> */}
     </main>
   )
 }
